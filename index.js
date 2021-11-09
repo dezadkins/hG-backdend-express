554d2776b9c75d6e8ad2f60953f28d0ae9bf7b5e
Dez <desmondadkins@gmail.com>
2021-11-09 13:48:13 -0800

    backend intial completion

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+
 import { posts } from "./routes/posts.js";
+import { MONGO_URI } from "./config/keys.js";
 
 const app = express();
 
 // CRUDING IS HAPPENING HERE GET, POST, PUT, PATCH, DELETE
 
 app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB & Hidden Gems Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/didwegetit", (req, res) => {
   res.send("We did get it! Wooohoo!");
