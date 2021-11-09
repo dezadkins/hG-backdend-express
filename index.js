a3d417098eede4f0188dec4e83e51ca7d5644bd1
Dez <desmondadkins@gmail.com>
2021-11-09 12:02:29 -0800

    first api endpoint tested

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+// CRUDING IS HAPPENING HERE GET, POST, PUT, PATCH, DELETE
 
-app.listen(port, () => console.log(`${port} is listening to you`));
+app.use(express.json());
+
+app.get("/didwegetit", (req, res) => {
+  res.send("We did get it! Wooohoo!");
+});
+
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => console.log(`${port} is listening to me`));
